7ebfe11fd45a67e3239911dc4af5fbc47a7fe48f
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-22 03:47:43 +0000

    lint express_server.js

--- express_server.js
+++ express_server.js
@@ -14,46 +14,46 @@ const urlDatabase = {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
-  res.render('urls_new')
-})
+  res.render('urls_new');
+});
 
 // User request short url
 // Add urls to urlDatabase
 app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString()
+  let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
-})
+});
 
 // Redirecting shortURL to longURL
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  console.log('longURL', longURL)
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log('longURL', longURL);
   if (longURL) {
     if (longURL === undefined) {
-      res.status(500).send('ERROR: BAD REQUEST ')
+      res.status(500).send('ERROR: BAD REQUEST ');
     }
-    res.redirect(longURL)
+    res.redirect(longURL);
   } else {
-    res.status(500).send('ERROR: I think you forgot to put http:// in longURL')
+    res.status(500).send('ERROR: I think you forgot to put http:// in longURL');
   }
-})
+});
 
 app.get(`/urls/:shortURL`, (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   const templateVars = { shortURL, longURL };
-  if(urlDatabase[shortURL]){
-  res.render('urls_show', templateVars)
+  if (urlDatabase[shortURL]) {
+    res.render('urls_show', templateVars);
   } else {
-    res.status(404).send('Error 404: Page Not Found')
+    res.status(404).send('Error 404: Page Not Found');
   }
-})
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -72,7 +72,7 @@ app.listen(PORT, () => {
 });
 
 
-function generateRandomString() {
+const generateRandomString = function () {
   let result = '';
   const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   for (let i = 0; i < 6; i++) {
